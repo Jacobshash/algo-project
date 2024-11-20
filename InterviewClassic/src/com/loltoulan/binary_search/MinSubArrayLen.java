8796d8d7bd429d90a402c31a8bb343cd6796cc0c
loltoulan <lol_toulan@163.com>
2024-11-20 23:03:25 +0800

    feat(InterviewClassic): 添加最小数组长度的三种解法
    - 暴力解法：minSubArrayLen1
    - 滑动窗口：minSubArrayLen2
    - 前缀和：minSubArrayLen
    
    同时，添加了 .gitignore 文件以忽略 out 目录

--- InterviewClassic/src/com/loltoulan/binary_search/MinSubArrayLen.java
+++ InterviewClassic/src/com/loltoulan/binary_search/MinSubArrayLen.java
@@ -5,21 +5,13 @@ import java.util.Arrays;
 public class MinSubArrayLen {
 
     public static void main(String[] args) {
-        int[] nums = {12, 28, 83, 4, 25, 26, 25, 2, 25, 25, 25, 12};
-        int target = 213;
+        int[] nums = {2, 3, 1, 2, 4, 3};
+        int target = 7;
         System.out.println(minSubArrayLen(target, nums));
     }
 
-    public static int minSubArrayLen(int target, int[] nums) {
-        if (Arrays.stream(nums).sum() < target) {
-            return 0;
-        }
-        if (Arrays.stream(nums).sum() == target) {
-            return nums.length;
-        }
-        if (Arrays.stream(nums).anyMatch(i -> i == target)) {
-            return 1;
-        }
+    // 暴力解法
+    public static int minSubArrayLen1(int target, int[] nums) {
         int n = nums.length;
         if (n == 0) {
             return 0;
@@ -38,4 +30,84 @@ public class MinSubArrayLen {
         return ans == Integer.MAX_VALUE ? 0 : ans;
     }
 
+    // 滑动窗口
+    public static int minSubArrayLen2(int target, int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int start = 0, end = 0;
+        int ans = Integer.MAX_VALUE;
+        int sum = 0;
+        while (end < n) {
+            sum = sum + nums[end];
+            if (sum >= target) {
+                ans = Math.min(ans, (end - start) + 1);
+                sum = sum - nums[start] - nums[end];
+                ++start;
+            } else {
+                ++end;
+            }
+
+        }
+        return ans == Integer.MAX_VALUE ? 0 : ans;
+    }
+
+
+    // 前缀和
+    public static int minSubArrayLen(int target, int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int ans = Integer.MAX_VALUE;
+        int[] sums = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            // 前缀和
+            sums[i] = sums[i - 1] + nums[i - 1];
+        }
+
+        for (int i = 1; i <= n; i++) {
+            int sum = sums[i-1] + target;
+            int bound = Arrays.binarySearch(sums, sum);
+            if (bound < 0) {
+                bound = -bound - 1; // 转换为插入点
+            }
+            if (bound <= n) {
+                ans = Math.min(ans, bound - (i - 1));
+            }
+        }
+        return ans == Integer.MAX_VALUE ? 0 : ans;
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
