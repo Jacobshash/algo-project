a736c95295af6602e6745f81fe78bcbf5f282a1d
LOL_toulan <lol_toulan@163.com>
2024-11-26 13:46:07 +0800

    feat(MergeIntervals): 添加官方解法并优化双指针算法
    - 添加官方解法 merge2 方法，使用 List 存储合并后的区间
    - 优化双指针算法，合并区间时考虑更多边界情况
    - 更新示例输入，增加更多测试用例

--- InterviewClassic/src/com/loltoulan/array/MergeIntervals.java
+++ InterviewClassic/src/com/loltoulan/array/MergeIntervals.java
@@ -1,34 +1,54 @@
 package com.loltoulan.array;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 public class MergeIntervals {
     public static void main(String[] args) {
-        int[][] intervals = {{1,3},{2,6},{18,20},{15,18}};
+        int[][] intervals = {{1, 3}, {2, 4}, {5, 14}, {15, 18}, {16, 20}};
         int[][] merge = merge(intervals);
         System.out.println(Arrays.deepToString(merge));
     }
 
-    public static int[][] merge(int[][] intervals) {
-        if (intervals.length <= 1) return intervals;
-        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
-        int[][] res = new int[intervals.length][2];
-        int startStart = 0;
 
-        int endStart = 1;
-        while (endStart < intervals.length) {
-            int startStartValue = intervals[startStart][0];
-            int startEndValue = intervals[startStart][1];
-
-            int endStartValue = intervals[endStart][0];
-            int endEndValue = intervals[endStart][0];
-
-            // if(){
-            //
-            // }
-
+    // 官方解法
+    public static int[][] merge2(int[][] intervals) {
+        if (intervals.length <= 1) {
+            return intervals;
         }
-        return res;
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+        List<int[]> merged = new ArrayList<>();
+        for (int[] interval : intervals) {
+            int L = interval[0], R = interval[1];
+            if (merged.isEmpty() || merged.getLast()[1] < L) {
+                merged.add(new int[] {L, R});
+            } else {
+                merged.getLast()[1] = Math.max(merged.getLast()[1], R);
+            }
+        }
+        return merged.toArray(new int[merged.size()][]);
+    }
+
+    // 双指针
+    public static int[][] merge(int[][] intervals) {
+        if (intervals.length <= 1) {
+            return intervals;
+        }
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+        List<int[]> merged = new ArrayList<>();
+
+        for (int i = 0; i < intervals.length; i++) {
+            int pre = intervals[i][1];
+            int j = i + 1;
+            while (j < intervals.length && intervals[j][0] <= pre) {
+                pre = Math.max(pre, intervals[j][1]);
+                j++;
+            }
+            merged.add(new int[] {intervals[i][0], pre});
+            i = j - 1;
+        }
+        return merged.toArray(new int[merged.size()][]);
     }
 }
