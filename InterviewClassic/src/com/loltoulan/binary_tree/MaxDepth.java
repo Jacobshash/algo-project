262a9a6442d84507c26aebd912ba871e0ca09775
LOL_toulan <lol_toulan@163.com>
2024-12-09 15:28:45 +0800

    feat(binary_tree): 二叉树最大深度算法
    - 新增广度优先搜索（BFS）和深度优先搜索（DFS）两种方法实现二叉树最大深度计算
    - 添加测试用例以验证算法正确性
    - 优化了原有代码结构，提高了可读性和可维护性

--- InterviewClassic/src/com/loltoulan/binary_tree/MaxDepth.java
+++ InterviewClassic/src/com/loltoulan/binary_tree/MaxDepth.java
@@ -1,32 +1,88 @@
 package com.loltoulan.binary_tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class MaxDepth {
 
     public static void main(String[] args) {
+        // TreeNode root = new TreeNode(1);
+        // root.left = new TreeNode(2);
+        // root.right = new TreeNode(3);
+        // root.left.left = new TreeNode(4);
+        // root.left.right = new TreeNode(5);
+        // root.right.left = new TreeNode(6);
+        // root.right.right = new TreeNode(7);
+        // root.right.right.right = new TreeNode(7);
+        // root.right.right.right.right = new TreeNode(7);
+        // root.left.left.left = new TreeNode(8);
+        // root.left.left.left.left = new TreeNode(8);
+        // System.out.println(maxDepth(root));
+
+        // TreeNode root = new TreeNode(3);
+        // root.left = new TreeNode(9);
+        // root.right = new TreeNode(20);
+        // root.right.left = new TreeNode(15);
+        // root.right.right = new TreeNode(7);
+        // System.out.println(maxDepth(root));
+
         TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
         root.right = new TreeNode(3);
         root.left.left = new TreeNode(4);
-        root.left.right = new TreeNode(5);
-        root.right.left = new TreeNode(6);
-        root.right.right = new TreeNode(7);
+        root.right.right = new TreeNode(5);
         System.out.println(maxDepth(root));
     }
 
-    public static int maxDepth(TreeNode root) {
+    // 广度优先
+    public static int maxDepthFromBFS(TreeNode root) {
         if (root == null) {
             return 0;
         }
         if (root.left == null && root.right == null) {
             return 1;
         }
-        int leftDepth = 1;
-        int rightDepth = 1;
-        while (root.left != null || root.right != null) {
-
+        int deepth = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                TreeNode pop = queue.poll();
+                if (pop != null) {
+                    if (pop.left != null) {
+                        queue.offer(pop.left);
+                    }
+                    if (pop.right != null) {
+                        queue.offer(pop.right);
+                    }
+                }
+            }
+            deepth++;
         }
-        return 0;
+        return deepth;
     }
+
+    // 深度优先
+    public static int maxDepthFromDFS(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        return dfs(root);
+    }
+
+    public static int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDeep = dfs(root.left);
+        int rightDeep = dfs(root.right);
+        return Math.max(leftDeep, rightDeep) + 1;
+    }
+
+
 
 }
